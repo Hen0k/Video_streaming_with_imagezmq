c39a425e119c83f2c64c46454a08986565d39f91
Henok <henoktilaye7@gmail.com>
2019-06-02 14:44:53 +0300

    cleaner start

--- server.py
+++ server.py
@@ -19,30 +19,8 @@ import cv2
 # initialize the ImageHub object
 imageHub = imagezmq.ImageHub()
 
-frameDict = {}
 
-# initialize the dictionary which will contain  information regarding
-# when a device was last active, then store the last time the check
-# was made was now
-lastActive = {}
-lastActiveCheck = datetime.now()
- 
-# stores the estimated number of Pis, active checking period, and
-# calculates the duration seconds to wait before making a check to
-# see if a device was active
-ESTIMATED_NUM_PIS = 2
-ACTIVE_CHECK_PERIOD = 10
-ACTIVE_CHECK_SECONDS = ESTIMATED_NUM_PIS * ACTIVE_CHECK_PERIOD
- 
-# assign montage width and height so we can view all incoming frames
-# in a single "dashboard"
-"""
-mW = args["montageW"]
-mH = args["montageH"]
-print("[INFO] detecting: {}...".format(", ".join(obj for obj in
-	CONSIDER)))
-"""
-#e video streaming over network with OpenCV and ImageZMQPython
+# video streaming over network with OpenCV and ImageZMQPython
 
 # start looping over all the frames
 while True:
@@ -51,16 +29,9 @@ while True:
     (rpiName, frame) = imageHub.recv_image()
     imageHub.send_reply(b'OK')
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
+    cv2.imshow('From %s' % rpiName, frame)
 	
     c = cv2.waitKey(1)
     if c == 27:
         break
-	# if a device is not in the last active dictionary then it means
-	# that its a newly connected device
-    if rpiName not in lastActive.keys():
-        print("[INFO] receiving data from {}...".format(rpiName))
- 
-	# record the last active time for the device from which we just
-	# received a frame
-    lastActive[rpiName] = datetime.now()
+
