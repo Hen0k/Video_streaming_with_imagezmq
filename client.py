ba9ba2058fcfa0b7a0f6880506a8c7a877422b64
Henok <henoktilaye7@gmail.com>
2019-06-03 12:19:46 +0300

    final commit

--- client.py
+++ client.py
@@ -9,21 +9,68 @@ Created on Sat Jun  1 22:03:04 2019
 # import the necessary packages
 from imutils.video import VideoStream
 import imagezmq
-import argparse
 import socket
 import time
+import cv2
 
-sender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:5555")
+
+def initSender(out_port=5051):
+    frameSender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:%d" % out_port)
+    senderName = socket.gethostname()
+
+    return frameSender, senderName
+
+def startCapture(delay=2.0):
+    print("Getting Camera")
+    videoStream = VideoStream(src=0).start()
+    time.sleep(delay)
+    print("Got Camera")
+
+    return videoStream
+
+def sendframe(sender, vs, name):
+    frame = vs.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    hub = sender.send_image(name, frame)
+    print(frame.shape)
+
+    return 0
+
+def initReciver(in_port=5050):    
+    # initialize the ImageHub object
+    imageHub = imagezmq.ImageHub(open_port='tcp://127.0.0.1:%d' %in_port)
+
+    return imageHub
+
+def reciveandshow(hub):
+    # receive sender host name and frame from the sender
+    # and acknowledge the receiption
+    (rpiName, frame) = hub.recv_image()
+    hub.send_reply(b'OK')
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    cv2.imshow('From %s (server)' % rpiName, frame)
+	
+    c = cv2.waitKey(1)
+    if c == 27:
+        return -1
+
+    return 0
+
+# Sending
+frameSender, senderName = initSender()
+vs = startCapture()
+
+# Reciving
+frameHub = initReciver()
 
 
-# get the host name, initialize the video stream, and allow the
-# camera sensor to warmup
-rpiName = socket.gethostname()
-# vs = VideoStream(usePiCamera=True).start()
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
- 
 while True:
-	# read the frame from the camera and send it to the server
-	frame = vs.read()
-	sender.send_image(rpiName, frame)
+    # Send
+    sendframe(frameSender, vs, senderName)
+
+    # Recive
+    reciveandshow(frameHub)
+
+   
+    
+
--- server.py
+++ server.py
@@ -7,31 +7,70 @@ Created on Sat Jun  1 22:15:58 2019
 """
 
 # import the necessary packages
-from imutils import build_montages
-from datetime import datetime
-import numpy as np
+from imutils.video import VideoStream
 import imagezmq
-import argparse
-import imutils
+import socket
+import time
 import cv2
 
+def initSender(out_port=5050):
+    frameSender = imagezmq.ImageSender(connect_to="tcp://127.0.0.1:%d" % out_port)
+    senderName = socket.gethostname()
 
-# initialize the ImageHub object
-imageHub = imagezmq.ImageHub()
+    return frameSender, senderName
 
+def startCapture(delay=2.0):
+    print("getting camera")
+    # here src=2 is for my usb webcam. yours can be 1 or 2 or 3 and so on for usb
+    # and 0 or -1 for built in webcam
+    videoStream = VideoStream(src=2).start()
+    print("got camera")
+    time.sleep(delay)
 
-# video streaming over network with OpenCV and ImageZMQPython
+    return videoStream
 
-# start looping over all the frames
-while True:
-	# receive RPi name and frame from the RPi and acknowledge
-	# the receipt
-    (rpiName, frame) = imageHub.recv_image()
-    imageHub.send_reply(b'OK')
+def sendframe(sender, vs, name):
+    frame = vs.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    sender.send_image(name, frame)
+    print(frame.shape)
+    return 0
+
+def initReciver(in_port=5051):    
+    # initialize the ImageHub object
+    imageHub = imagezmq.ImageHub(open_port='tcp://127.0.0.1:%d' %in_port)
+
+    return imageHub
+
+def reciveandshow(hub):
+    # receive sender host name and frame from the sender
+    # and acknowledge the receiption
+    (rpiName, frame) = hub.recv_image()
+    hub.send_reply(b'OK')
+    
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    cv2.imshow('From %s' % rpiName, frame)
+    cv2.imshow('From %s (client)' % rpiName, frame)
 	
     c = cv2.waitKey(1)
     if c == 27:
-        break
+        return -1
 
+    return 0
+
+
+# Sending
+frameSender, senderName = initSender()
+vs = startCapture()
+
+
+
+
+# video streaming over network with OpenCV and ImageZMQPython
+frameHub = initReciver()
+
+while True:
+    #Reciving
+    reciveandshow(frameHub)
+
+    # Sending
+    sendframe(frameSender, vs, senderName)
